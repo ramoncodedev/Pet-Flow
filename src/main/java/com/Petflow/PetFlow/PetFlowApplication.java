4a11d4dcc079b4153d14ce25b570801a4acb5614
ramoncodedev <ramonwebdesigner2025@gmail.com>
2026-01-12 20:45:03 -0300

    Inicializando o sistema

---
+++ src/main/java/com/Petflow/PetFlow/PetFlowApplication.java
@@ -0,0 +1,13 @@
+package com.Petflow.PetFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Petflow/PetFlow/PetFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Petflow.PetFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
